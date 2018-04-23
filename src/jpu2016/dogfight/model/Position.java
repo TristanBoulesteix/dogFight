3373c26be03d2211e12b296b10fe137b6f778603
TristanBoulesteix <tristan.boulesteix@gmail.com>
2018-04-23 15:46:35 +0200

    null

--- src/jpu2016/dogfight/model/Position.java
+++ src/jpu2016/dogfight/model/Position.java
@@ -1,9 +1,42 @@
 package jpu2016.dogfight.model;
 
 public class Position {
+	private double x;
+	private double y;
 
-	public Position() {
-		// TODO Auto-generated constructor stub
+	private double maxX;
+	private double maxY;
+
+	public Position(double x, double y, double maxX, double maxY) {
+
+	}
+
+	public Position(Position position) {
+
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	protected void setMaxX(double maxX) {
+		this.maxX = maxX;
+	}
+
+	protected void setMaxY(double maxY) {
+		this.maxY = maxY;
 	}
 
 }
---
+++ src/jpu2016/dogfight/model/Sky.java
@@ -0,0 +1,20 @@
+package jpu2016.dogfight.model;
+
+import java.awt.Image;
+
+public class Sky implements IArea {
+
+	public Sky(Dimension dimension) {
+
+	}
+
+	@Override
+	public Dimension getDimension() {
+
+	}
+
+	@Override
+	public Image getImage() {
+
+	}
+}
