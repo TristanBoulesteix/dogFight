36ef58870faecb573bc5b7088ca840b43ea5f55d
RPapin <33548867+RPapin@users.noreply.github.com>
2018-04-23 17:02:37 +0200

    a
    a

--- src/jpu2016/dogfight/gameframe/GameFrame.java
+++ src/jpu2016/dogfight/gameframe/GameFrame.java
@@ -1,17 +1,5 @@
-package dogfight.gameframe;
+package jpu2016.dogfight.gameframe;
 
-public class GameFrame extends JFrame{
-public GameFrame(String title, IEventPerformer performer, IGraphicsBuilder graphicBuilder, Observable observable) {
-	
-}
-public void keyPressed(KeyEvent keyEvent) {
-	
-}
-public void keyReleased(KeyEvent keyEvent) {
-
-}
-public void keyTyped(KeyEvent keyEvent) {
-	
-}
+public class GameFrame {
 
 }
